7bd4a5c3252a8af7a42aee007da5c4bc426f681b
richard-to <gohn76@gmail.com>
2013-10-19 21:32:53 -0800

    Increase default depth to 17

--- pa3/java/src/to/richard/owari/gui/GameBoard.java
+++ pa3/java/src/to/richard/owari/gui/GameBoard.java
@@ -36,7 +36,7 @@ public class GameBoard extends JFrame {
 
     public static final int DEPTH_MIN = 0;
     public static final int DEPTH_MAX = 100;
-    public static final int DEPTH_DEFAULT = 16;
+    public static final int DEPTH_DEFAULT = 17;
     public static final int DEPTH_MAJOR_TICK = 5;
     public static final int DEPTH_MINOR_TICK = 1;
 
