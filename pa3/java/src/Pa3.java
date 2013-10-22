2c95c14bd9fa468cde2f88e08d5f98dd8d0497f3
richard-to <gohn76@gmail.com>
2013-10-22 13:34:26 -0800

    Clean up PA3 main

--- pa3/java/src/Pa3.java
+++ pa3/java/src/Pa3.java
@@ -1,9 +1,10 @@
 import to.richard.owari.GameEngine;
-import to.richard.owari.Owari;
 import to.richard.owari.ai.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 import java.util.Properties;
 
 /**
@@ -11,21 +12,48 @@ import java.util.Properties;
  * Date: 10/11/13
  */
 public class Pa3 {
-    public static final boolean USE_CACHE_PLAYER = false;
+    public static final int CACHE_SOCKET_PORT = 5007;
 
     public static final String CONFIG = "db.properties";
     public static final String CONFIG_PROP_CONN_STRING = "connection_string";
 
-    public static void main(String[] args) {
-        if (USE_CACHE_PLAYER) {
-            runAlphaBetaCachePlayer();
-        } else {
-            IComputerAi ai = new AlphaBetaPlayerID();
-            GameEngine gameEngine = new GameEngine(ai);
-        }
+    public static void main(String[] args) throws IOException {
+        runNormal();
+        //runWithIterativeDeepening();
     }
 
-    public static void runAlphaBetaCachePlayer() {
+    public static void runNormal() {
+        IComputerAi ai = new AlphaBetaPlayer();
+        GameEngine gameEngine = new GameEngine(ai);
+    }
+
+    public static void runWithIterativeDeepening() {
+        IComputerAi ai = new AlphaBetaPlayerID();
+        GameEngine gameEngine = new GameEngine(ai);
+    }
+
+    public static void runWithTranspositionTable() throws IOException {
+        SocketChannel[] scs = new SocketChannel[6];
+        for (int i = 0; i < scs.length; i++) {
+            SocketChannel sc = null;
+            sc = SocketChannel.open();
+            sc.configureBlocking(false);
+            sc.connect(new InetSocketAddress(5007));
+            while (!sc.finishConnect()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            scs[i] = sc;
+        }
+
+        IComputerAi ai = new AlphaBetaPlayerTT(scs);
+        GameEngine gameEngine = new GameEngine(ai);
+    }
+
+    public static void runWithSqlLiteCache() {
         Properties prop = new Properties();
         String connectionString = null;
         try {
