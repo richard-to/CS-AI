2d33fef352e99564d100d939373cb96260348b56
richard-to <gohn76@gmail.com>
2013-11-04 17:32:30 -0800

    Temp commit. Buggy...

--- pa4/ga.py
+++ pa4/ga.py
@@ -18,19 +18,33 @@ def genInitialPop(popSize, maxWeight, objList):
     return population
 
 
+def tournamentSelection(population):
+    popSize = len(population)
+    newPopulation = []
+    for i in xrange(popSize):
+        genome1 = population[random.randint(0, popSize - 1)]
+        genome2 = population[random.randint(0, popSize - 1)]
+        if genome1[0] > genome2[0]:
+            newPopulation.append([genome1[0], genome1[1][:]])
+        else:
+            newPopulation.append([genome2[0], genome2[1][:]])
+    return newPopulation
+        
+
+    
 def mutate(parent1, mutationPct, maxWeight, objList):
     child1 = parent1[1][:]
     child1Weight = 0
     child1Value = 0
 
-    for i in xrange(child1):
+    for i in xrange(len(child1)):
         if random.random() < mutationPct:
             if child1[i] == 0:
                 child1[i] = 1
             else:
                 child1[i] = 0
 
-    for i in xrange(child1):
+    for i in xrange(len(child1)):
         if child1[i] > 0:
             child1Weight += objList[i][0]
             child1Value += objList[i][1]
@@ -51,7 +65,7 @@ def crossover(parent1, parent2, maxWeight, objList):
     child2Value = 0
     child2Weight = 0
 
-    for i in xrange(objList):
+    for i in xrange(len(objList)):
         if child1[i] > 0:
             child1Weight += objList[i][0]
             child1Value += objList[i][1]
@@ -73,24 +87,49 @@ def crossover(parent1, parent2, maxWeight, objList):
 
 
 def main():
-    random.seed(10)
+    #random.seed(10)
     numIterations = 10
     maxWeight = 100
-    popSize = 3
-    mutationPct = 0.1
+    popSize = 4
+    mutationPct = 0.05
     objList = [
         [45, 3],
         [40, 5],
         [50, 8],
         [90, 10]
     ]
-    print len(objList)-2
 
+    print "Starting population"
     population = genInitialPop(popSize, maxWeight, objList)
     print population
 
-    crossover(population[0], population[1], maxWeight, objList)
+    currentGen = 0
+    while currentGen < numIterations:
+        print "Generation ", currentGen
+        print "Selection"
+        newPopulation = tournamentSelection(population)
+        print newPopulation
 
+        recombinedPop = []
+        while len(recombinedPop) < popSize:
+            child1 = newPopulation[random.randint(0, popSize - 1)]
+            child2 = newPopulation[random.randint(0, popSize - 1)]
+            recombinedChildren = crossover(child1, child2, maxWeight, objList)
+            recombinedPop.append(recombinedChildren[0])
+            recombinedPop.append(recombinedChildren[1])
 
+        print "After crossover"
+        print recombinedPop
+
+        mutatedPop = []
+        for i in xrange(len(recombinedPop)):
+            mutatedPop.append(
+                mutate(recombinedPop[i], mutationPct, maxWeight, objList))
+        print "After mutation"
+        print mutatedPop
+        currentGen += 1
+        
+    print "Final Population"
+    print mutatedPop
 if __name__ == '__main__':
     main()
